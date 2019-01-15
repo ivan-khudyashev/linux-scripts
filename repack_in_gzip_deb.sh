35dd193eda6fb49326f2ac910fc92a205b311723
Khudyashev Ivan <bahek1983@gmail.com>
2019-01-15 18:23:06 +0700

    Add repack deb script
    Script repack deb-packages with gzip compression.
    This is necessary for creation deb for old `dpkg` version, which
     can not work with new deb-package format.

---
+++ repack_in_gzip_deb.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# This script repack deb-packages in deb with same name but packed with gzip
+# This is necessary if used old `dpkg` utility with new deb-packages
+TMPDIR=/tmp/dpkg_repack_dir
+DEBFILE=
+# 0. Preliminary test if argument is supplied
+if [ -z "$1" ]
+then
+    echo "It is necessary supply first argument: deb-package for repack"
+    exit 1
+fi
+# 1. Now check if first arg is existing file
+if [ -n "$1"  ]
+then
+    if [ -f "$1"  ]
+    then
+        DEBFILE="$1"
+    fi
+fi
+if [ -z ${DEBFILE} ]
+then
+    echo "Deb-package file < $1 > not exists"
+    exit 1
+fi
+#TODO: check found deb-file by extension: that this exactly deb
+#TODO: check found deb-file by content: that this exactly deb
+# 3. Check if tmp directory exists
+if [ -n  ${TMPDIR} ]
+then
+    rm -rf ${TMPDIR}
+fi
+# 4. Create temp-directory for work
+mkdir ${TMPDIR}
+# 5. Extract and unpack deb-package in temp-directory
+dpkg-deb -x "${DEBFILE}" "${TMPDIR}/"
+dpkg-deb -e "${DEBFILE}" "${TMPDIR}/DEBIAN"
+# 6. Repack deb-package with gzip
+dpkg-deb -Zgzip -b "${TMPDIR}/" .
+echo "Repack ${DEBFILE}"
