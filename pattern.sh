e176d83702caec82dabc0ac775cccc4ce7af3e9d
Khudyashev Ivan <bahek1983@gmail.com>
2019-02-08 14:31:09 +0700

    Add pattern script

---
+++ pattern.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Pattern-script
+# Section for describe Aims
+# Section for short info
+# 0. Define variables
+# 1. Check input params if exists
+# 2. Start actions
+echo "This just pattern-script"
