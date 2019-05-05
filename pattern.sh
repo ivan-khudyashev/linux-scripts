7f431ac98bc68afbdc3e2fcce0a591adcc6278b0
Khudyashev Ivan <bahek1983@gmail.com>
2019-05-05 14:16:01 +0700

    Add save header in pattern and cur.script

--- pattern.sh
+++ pattern.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
-# Pattern-script
+#!/usr/bin/env bash
+set -o nounset # save for using undeclare variable
+set -o errexit # immediately exit on error, save from continue work on error
 # Section for describe Aims
 # Section for short info
 # 0. Define variables
